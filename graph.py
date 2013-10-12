984757eeafc18bad528040d2208ad233527c1aab
Michael Langston <mnlangston@gmail.com>
2013-10-12 17:23:53 -0500

    DFS implemented

--- graph.py
+++ graph.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 class Node:
 	def __init__(self,label):
 		self.label=label
@@ -15,6 +15,8 @@ class Graph:
 			if i.label is label:
 				return i
 		return None
+	def first(self):
+		return self.nodes[0]
 
 	def printMap(self):
 		print "=== constructed table ==="
@@ -39,8 +41,28 @@ class Graph:
 		#add children
 		for i in self.nodes:
 			for j in self.nodemap[i.label]:
+
 				i.children.append(self.find(j))
+	def depthFirstSearch(self,val,start):
+		visited = [] 
+		stack = []
+		current = None
+		stack.append(start)
+		while stack:
+			current = stack.pop()
+			visited.append(current)
+			for c in sorted(current.children,reverse=True):
+				if c.label is val: 
+					return True
+				if c not in visited and c not in stack: 
+					stack.append(c)
+		return False
 
 if __name__=="__main__":
 	g = Graph()
 	g.printMap()
+	val=raw_input("Search for what value: ")
+	if g.depthFirstSearch(val,g.first()):
+		print "Found it"
+	else:
+		print "Not found"
